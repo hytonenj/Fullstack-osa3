009ae8a4ef48a34b74f99580f392b0cbcf0511c6
Jimmeeee <hytonenjimi@gmail.com>
2019-05-30 15:23:05 +0300

    tehtava3.20

--- models/person.js
+++ models/person.js
@@ -14,9 +14,10 @@ mongoose.connect(url, { useNewUrlParser: true })
   })
 
 const personSchema = new mongoose.Schema({
-  name: { type: String, required: true, unique: true },
-  number: { type: String, required: true }
+  name: { type: String, required: true, unique: true, minlength: 3 },
+  number: { type: String, required: true, minlength: 8 }
 })
+
 personSchema.plugin(uniqueValidator)
 
 personSchema.set('toJSON', {
